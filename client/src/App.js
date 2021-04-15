0d4544c5d86e5a6f139f85f32a20e18338206a70
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-04-15 20:50:22 +0530

    navbar

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+import {Fragment} from 'react'
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar/>
+    </Fragment>
+    
+  );
+}
+
+export default App;
